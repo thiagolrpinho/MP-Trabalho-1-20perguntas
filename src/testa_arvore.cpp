e03f89dee6d3736ee06e68392ac8eaef6cc898c2
thiagolrpinho <thiagoluisrpinho@gmail.com>
2018-03-30 18:23:24 -0300

    Adicionadas guardas de incorporação ao testa_arvore

--- src/testa_arvore.cpp
+++ src/testa_arvore.cpp
@@ -1,6 +1,11 @@
-
+#ifndef CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#endif  // CATCH_CONFIG_MAIN
+
+#ifndef  CATCH_H_INCLUDED   // SE CATCH JÁ TIVER SIDO INCORPORADO ANTERIORMENTE, ELE NÃO FAZ DE NOVO
+#define  CATCH_H_INCLUDED
 #include "catch.hpp"
+
 
 
 unsigned int Factorial( unsigned int number ) {
@@ -12,5 +17,9 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(0) == 1 );
+    REQUIRE_FALSE( Factorial(0) == 2 );
     REQUIRE( Factorial(10) == 3628800 );
 }
+
+
+#endif //CATCH_H_INCLUDED
