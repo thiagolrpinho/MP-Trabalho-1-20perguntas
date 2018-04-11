1fea978801b7676591a821a02dc1a84285b7161e
Thiago Luis <thiagoluisrpinho@gmail.com>
2018-04-11 15:47:17 -0300

    Green

---
+++ include/game_engine.hpp
@@ -0,0 +1,51 @@
+#ifndef GAME_QUESTION_HPP
+#define GAME_QUESTION_HPP
+
+#include "btree.hpp"
+
+typedef shared_ptr<StringNode> PStringNode;
+/*
+  This is library was built to abstract game engine in the game 20 questions.
+  Each node represents an statement. 
+  Trying to acess nullpointer is considered a don't know. The game should return
+  "don't know" to the user and he/she will be asked what were the right answer. 
+  A node with both branches null will be considered an Answer. If the answer
+  is right, the game ends. If it's not, the user'll be asked to write the right
+  one and the wrong answer will become a question of Yes or No which should
+  lead to the right answer.
+  A question is a node that is not null or have it's branches null.
+  The game engine have two pointers, one to the start of the game and another
+  to the actual node. 
+
+*/
+
+class GameEngine {
+ 
+ private:
+  shared_ptr<BTree> pTreeOfStatements;
+  PStringNode pActualNode;
+
+ public:
+ GameEngine();
+ GameEngine(string initialText);
+
+ //Positioning logic
+ PStringNode getStart(void);
+ PStringNode getActualNode(void);
+ int setActualNode( PStringNode pNextNode );
+
+ //Reading and writing values on nodes
+ string readActualNode( void );
+ int writeInActualNode( string newText );
+ 
+ //YesOrNo Logic
+ int newYesAnswer(void);
+ int newYesAnswer(string initialText);
+ int newYesQuestion(string initialQuestion);
+ PStringNode getYes(void);
+
+};
+
+
+
+#endif
---
+++ src/game_engine.cpp
@@ -0,0 +1,81 @@
+#include "game_engine.hpp"
+
+typedef shared_ptr<StringNode> PStringNode;
+
+GameEngine::GameEngine()
+{
+    pTreeOfStatements.reset(new BTree());
+    //Initial Actual Node is the start node
+    this->setActualNode(getStart());
+}
+
+GameEngine::GameEngine(string initialText)
+{
+    pTreeOfStatements.reset(new BTree(initialText));
+     //Initial Actual Node is the start node
+    this->setActualNode(getStart());
+}
+
+ PStringNode GameEngine::getStart(void)
+ { 
+     return pTreeOfStatements->getRoot();
+ };
+ PStringNode GameEngine::getActualNode(void)
+ {
+      return pActualNode; 
+ };
+ int GameEngine::setActualNode( PStringNode pNextNode )
+ { 
+    try {
+        pActualNode = pNextNode;
+    } catch (int e) {
+        return Error;
+    }
+    return Sucess;
+ };
+
+string GameEngine::readActualNode( void )
+{
+    return getActualNode()->getText();
+};
+
+int GameEngine::writeInActualNode( string newText )
+{
+    try {
+        return getActualNode()->setText( newText );
+    } catch(int e) {
+        return Error;
+    }
+};
+
+int GameEngine::newYesAnswer()
+{ 
+    pTreeOfStatements->getRoot()->insertLeftNode();
+    return Sucess; 
+};
+
+int GameEngine::newYesAnswer(string initialText)
+{ 
+    pTreeOfStatements->getRoot()->insertLeftNode(initialText);
+    return Sucess; 
+};
+
+int GameEngine::newYesQuestion(string initialQuestion)
+{
+    PStringNode newQuestionNode;
+    try{
+        newYesAnswer(initialQuestion);
+        newQuestionNode = this->getYes();
+        newQuestionNode->insertLeftNode();
+        newQuestionNode->insertRightNode();
+    } catch (int e){
+        return Error;
+    }
+    return Sucess;
+};
+
+shared_ptr<StringNode> GameEngine::getYes()
+{ 
+    return pTreeOfStatements->getRoot()->getLeftNode(); 
+};
+
