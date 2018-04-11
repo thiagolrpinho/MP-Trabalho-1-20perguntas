d9f6c2595fac4863ef0325b292d5cc090ef4ed79
Thiago Luis <thiagoluisrpinho@gmail.com>
2018-04-11 12:49:20 -0300

    #002_criando_biblioteca_de_arvore_binaria: Auto stash before revert of "Refactoring and still green"

---
+++ include/game_question.hpp
@@ -0,0 +1,22 @@
+#ifndef GAME_QUESTION_HPP
+#define GAME_QUESTION_HPP
+
+#include "btree.hpp"
+
+/*
+  This is library was built to abstract game questions in the game 20 questions
+*/
+
+class GameQuestion {
+ 
+ private:
+  shared_ptr<BTree> pBTree;
+
+ public:
+
+
+};
+
+
+
+#endif
---
+++ src/tests_game_question.cpp
@@ -0,0 +1,20 @@
+#include "game_question.hpp"
+
+typedef shared_ptr<GameQuestion> PGAMEQUESTION;
+/*
+THESE TESTS WILL BE FOCUSED ON THE  QUESTIONS OF 
+  THE 20_QUESTIONS GAME
+  It'll be consired fully functional if they pass on
+  four test cases:
+    Create questions, Read questions, update question
+    and delete questions
+*/
+
+TEST_CASE( "CREATE QUESTIONS", "[20_QUESTION_GAME_Questions]" )
+{
+  SECTION( "creating a question" )
+  {
+    PGAMEQUESTION pGameQuestion(new GameQuestion());
+    REQUIRE_FALSE( pGameQuestion == nullptr );
+  }
+}
