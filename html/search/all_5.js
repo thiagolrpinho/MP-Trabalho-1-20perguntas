1eadda64ddd150ca38c222244da095992a5568c2
Thiago Luis <thiagoluisrpinho@gmail.com>
2018-04-15 20:34:44 -0300

    Doxygen files

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['gameengine',['GameEngine',['../classGameEngine.html',1,'']]],
-  ['gameinterface',['GameInterface',['../classGameInterface.html',1,'']]]
+  ['gameinterface',['GameInterface',['../classGameInterface.html',1,'']]],
+  ['game_20of_2020_20questions',['Game of 20 Questions',['../md_readme.html',1,'']]]
 ];
